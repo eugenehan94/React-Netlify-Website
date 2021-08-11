15c1af65e10d7a0eabd8735cf26ad7c698f52cc4
eugenehan94 <eugene.han94@gmail.com>
2021-08-11 18:20:56 -0400

    phone animation added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,39 +1,54 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { FaPhoneAlt } from "react-icons/fa";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+
 import { Link } from "react-router-dom";
 import "../css/Contact.css";
+import { IconContext } from "react-icons";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
+
       <div className="Contact_inner_container">
-        <h1 className="Contact_title">Contact Us</h1>
-        <h1 className="Contact_title">Need to contact me</h1>
-        <p className="Contact_desc">Feel free to contact me on:</p>
+        <div className="Contact_left_container">
+          <h1 className="Contact_title">Contact Us</h1>
+          <h1 className="Contact_title">Need to contact me</h1>
+          <p className="Contact_desc">Feel free to contact me on:</p>
 
-        <a
-          className="Contact-linkedin-link"
-          href="http://www.linkedin.com/in/eugene-h-7928bb63"
-          target="_blank"
-        >
-          <AiFillLinkedin /> Linkedin
-        </a>
-        <br />
-        <a
-          href="https://github.com/eugenehan94"
-          target="_blank"
-          className="Contact-linkedin-link"
-        >
-          <AiFillGithub color="white" /> Github
-        </a>
+          <a
+            className="Contact-linkedin-link"
+            href="http://www.linkedin.com/in/eugene-h-7928bb63"
+            target="_blank"
+          >
+            <AiFillLinkedin /> Linkedin
+          </a>
+          <br />
+          <a
+            href="https://github.com/eugenehan94"
+            target="_blank"
+            className="Contact-linkedin-link"
+          >
+            <AiFillGithub color="white" /> Github
+          </a>
 
-        <div>
-          <Link to="/">
-            <button className="Contact_btn">Return Home</button>
-          </Link>
+          <div>
+            <Link to="/">
+              <button className="Contact_btn">Return Home</button>
+            </Link>
+          </div>
+        </div>
+        <div className="Contact_right_container">
+          <IconContext.Provider
+            value={{ className: "Contact_phone_animation" }}
+          >
+            <div>
+              <FaPhoneAlt />
+            </div>
+          </IconContext.Provider>
         </div>
       </div>
 
