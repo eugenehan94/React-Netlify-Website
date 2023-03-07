f67246d32716ff16a695ee9f96faeff5cd68f54b
eugenehan94 <eugene.han94@gmail.com>
2023-03-07 12:45:00 -0500

    started loading component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 /* NOTE: Keeping React Router, for future sakes if we introduce different pages*/
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Loading from "./components/loading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <Loading setLoading={setLoading}/>;
+  }
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home setLoading={setLoading}/>
         </Route>
       </Switch>
     </Router>
