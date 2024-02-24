ac0abc4530f49b70bfa0bd7a7f7669ee038809f7
eugenehan94 <eugene.han94@gmail.com>
2024-02-23 19:26:02 -0500

    refactored not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,37 +1,52 @@
-import { Box, Button, Typography, Grid } from "@mui/material";
+import { Box, Button, Typography, styled } from "@mui/material";
 import Navbar from "../components/_shared/navbar";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const StyledContainer = styled(Box)(({ theme }) => ({
+  backgroundColor: "#090e10",
+  height: "100%",
+  width: "100%",
+}));
+
+const StyledContentContainer = styled(Box)(() => ({
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}));
+
+const StyledTitleTypography = styled(Typography)(() => ({
+  color: "#ffffff",
+  fontSize: "150px",
+}));
+
+const StyledParagraphyTypography = styled(Typography)(() => ({
+  color: "#ffffff",
+  fontSize: "32px",
+}));
+
 const NotFound = () => {
   return (
-    <Box>
+    <StyledContainer>
       <Navbar />
-      <Box sx={{ backgroundColor: "#090e10", height: "100vh" }}>
-        <Box
-          sx={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography sx={{ color: "#ffffff", fontSize: "150px" }}>
-            404
-          </Typography>
-          <Typography sx={{ color: "#ffffff", fontSize: "32px" }}>
+      <Box sx={{ height: "100vh" }}>
+        <StyledContentContainer>
+          <StyledTitleTypography>404</StyledTitleTypography>
+          <StyledParagraphyTypography>
             Page Not Found
-          </Typography>
+          </StyledParagraphyTypography>
           <Button
             href="/"
             variant="contained"
             sx={{ margin: "3rem 0" }}
             endIcon={<ArrowForwardIcon />}
+            size="large"
           >
             Back Home
           </Button>
-        </Box>
+        </StyledContentContainer>
       </Box>
-    </Box>
+    </StyledContainer>
   );
 };
 
